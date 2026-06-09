8dbb7a6a9ad4b1b0de3a75f4f58bdb6c6f2a99de
mmarler <marcus.marler@austin.utexas.edu>
2026-06-09 13:23:40 -0500

    PR Changes

---
+++ src/Hook/Hooks.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Drupal\utexas_smtp_helper\Hook;
+
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Hook\Attribute\Hook;
+use Drupal\Core\StringTranslation\StringTranslationTrait;
+
+/**
+ * Hook implementations.
+ */
+class Hooks {
+
+  use StringTranslationTrait;
+
+  /**
+   * Implements hook_form_FORM_ID_alter().
+   */
+  #[Hook('form_smtp_admin_settings_alter')]
+  public function formSmtpAdminSettingsAlter(array &$form, FormStateInterface $form_state, string $form_id): void {
+    if (\Drupal::state()->get('utexas_smtp')) {
+      \Drupal::messenger()->addMessage($this->t("This module's configuration is being set by the UTexas SMTP Helper module."));
+    }
+  }
+
+}
