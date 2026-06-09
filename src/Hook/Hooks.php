6948ab18fb2d17a74df00a47ea44412529a7ebe0
mmarler <marcus.marler@austin.utexas.edu>
2026-06-09 16:06:58 -0500

    Log error in hook form alter

--- src/Hook/Hooks.php
+++ src/Hook/Hooks.php
@@ -14,22 +14,15 @@ class Hooks {
   use StringTranslationTrait;
 
   /**
-   * Implements hook_page_top().
-   */
-  #[Hook('page_top')]
-  public function pageTop(array &$page_top): void {
-    if (!function_exists('pantheon_get_secret')) {
-      \Drupal::logger('utexas_smtp_helper')->error("Can't override SMTP credentials: Pantheon secrets function not available.");
-    }
-  }
-
-  /**
    * Implements hook_form_FORM_ID_alter().
    */
   #[Hook('form_smtp_admin_settings_alter')]
   public function formSmtpAdminSettingsAlter(array &$form, FormStateInterface $form_state, string $form_id): void {
-    if (\Drupal::state()->get('utexas_smtp')) {
+    if (function_exists('pantheon_get_secret')) {
       \Drupal::messenger()->addMessage($this->t("This module's configuration is being set by the UTexas SMTP Helper module."));
+    }
+    else {
+      \Drupal::logger('utexas_smtp_helper')->error("Can't override SMTP credentials: Pantheon secrets function not available.");
     }
   }
 
